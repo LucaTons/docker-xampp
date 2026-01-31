fcdadf92ab19bf8c7faf2c8bab04aed093e199ca
st11004 <st11004@iismarconipieralisi.it>
2026-01-31 12:05:17 +0000

    Aggiornamento del codice

--- src/Chatrooms/chat.php
+++ src/Chatrooms/chat.php
@@ -8,7 +8,8 @@ if ($_SESSION && isset($_SESSION['username']))
     if ($_GET && isset($_GET['room_id']))
     {
         $stanza = $_GET['room_id'];
-        echo "Messaggi della stanza $stanza";
+        $nome = $_GET['NomeStanza'];
+        echo "Messaggi della stanza $nome";
         echo "<br>";
         //query dalla tabella messaggi e visualizzo i contenuti
         $query = "SELECT * FROM Messaggi WHERE NomeStanza = $stanza";
@@ -30,3 +31,19 @@ $connection->close();
 ?>
 
 <!-- form per inviare i messaggi alla chat -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="messaggio">Messaggio</label>
+        <input type="text" id="messaggio" name="messaggio">
+        <br>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
